bf364af1bd25531f67aa04e49c9c3f6bccf94b00
JeD <jedartois@gmail.com>
2013-10-01 10:37:42 +0200

    add MavenResolver

--- maven-resolver-cpp/src/MavenVersionResolver.h
+++ maven-resolver-cpp/src/MavenVersionResolver.h
@@ -5,41 +5,39 @@
 #include "MavenVersionResult.h"
 #include <string>
 
-#define buildLatestTag = "<latest>";
-#define buildEndLatestTag = "</latest>";
+#define buildLatestTag  "<latest>"
+#define buildEndLatestTag  "</latest>"
 
-#define buildReleaseTag = "<release>";
-#define buildEndreleaseTag = "</release>";
+#define buildReleaseTag  "<release>"
+#define buildEndreleaseTag  "</release>"
 
-#define buildMavenTag = "<buildNumber>";
-#define buildEndMavenTag = "</buildNumber>";
+#define buildMavenTag  "<buildNumber>"
+#define buildEndMavenTag  "</buildNumber>"
 
-#define timestampMavenTag = "<timestamp>";
-#define timestampEndMavenTag = "</timestamp>";
+#define timestampMavenTag  "<timestamp>"
+#define timestampEndMavenTag  "</timestamp>"
 
-#define lastUpdatedMavenTag = "<lastUpdated>";
-#define lastUpdatedEndMavenTag = "</lastUpdated>";
+#define lastUpdatedMavenTag  "<lastUpdated>"
+#define lastUpdatedEndMavenTag  "</lastUpdated>"
 
-#define snapshotVersionClassifierMavenTag = "<classifier>";
-#define snapshotVersionClassifierEndMavenTag = "</classifier>";
+#define snapshotVersionClassifierMavenTag  "<classifier>"
+#define snapshotVersionClassifierEndMavenTag  "</classifier>"
 
-#define snapshotVersionExtensionMavenTag = "<extension>";
-#define snapshotVersionExtensionEndMavenTag = "</extension>";
+#define snapshotVersionExtensionMavenTag  "<extension>"
+#define snapshotVersionExtensionEndMavenTag  "</extension>"
 
-#define snapshotVersionValueMavenTag = "<value>";
-#define snapshotVersionValueEndMavenTag = "</value>";
+#define snapshotVersionValueMavenTag  "<value>"
+#define snapshotVersionValueEndMavenTag  "</value>"
 
-#define snapshotVersionUpdatedMavenTag = "<updated>";
-#define snapshotVersionUpdatedEndMavenTag = "</updated>";
+#define snapshotVersionUpdatedMavenTag  "<updated>"
+#define snapshotVersionUpdatedEndMavenTag  "</updated>"
 
-#define metaFile = "maven-metadata.xml";
-#define localmetaFile = "maven-metadata-local.xml";
+#define metaFile "maven-metadata.xml"
+#define localmetaFile  "maven-metadata-local.xml"
 
-#if defined(WIN32) || defined(_WIN32) 
-#define PATH_SEPARATOR "\\" 
-#else 
+
 #define PATH_SEPARATOR "/" 
-#endif 
+
 
 
 class MavenVersionResolver
@@ -48,6 +46,7 @@ public:
 	MavenVersionResolver();
 	virtual ~MavenVersionResolver();
 	MavenVersionResult* resolveVersion(MavenArtefact artefact, std::string basePath, bool localDeploy);
+	std::string foundRelevantVersion(MavenArtefact artefact, std::string basePath, bool localDeploy);
 };
 
 #endif /*MAVENVERSIONRESOLVER_H_*/
---
+++ maven-resolver-cpp/src/test.cpp
@@ -0,0 +1,21 @@
+
+
+#include "MavenResolver.h"
+
+
+int main(int argc, char **argv) {
+	
+	MavenResolver *resolver = new MavenResolver();
+	
+	
+	
+	std::vector<std::string> urls;
+	urls.push_back("http://oss.sonatype.org/content/groups/public");
+	
+	
+	
+	FILE *result = resolver->resolve("org.kevoree","org.kevoree.core","RELEASE","jar", urls);
+	
+	
+	
+}
