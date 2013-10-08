b7794d053a27066e2ddfa1cef53fa4837b5e4b4f
JeD <jedartois@gmail.com>
2013-10-08 15:17:56 +0200

    add initial model transform

--- org.kevoree.iot.modelTransform/src/main/scala/org.kevoree.iot.modelTransform/AdaptationModelWrapper.scala
+++ org.kevoree.iot.modelTransform/src/main/scala/org.kevoree.iot.modelTransform/AdaptationModelWrapper.scala
@@ -1,37 +1,16 @@
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * 	http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.kevoree.iot.modelTransform
 
-import org.kevoree.tools.marShell.ast._
+
 import org.kevoreeadaptation._
 import org.kevoree.kompare.JavaSePrimitive
 import org.kevoree._
 import scala.collection.JavaConversions._
 import org.kevoree.log.Log
+import org.kevoree.tools.marShell.ast._
+import org.kevoree.tools.marShell.ast.MergeStatement
+import org.kevoree.tools.marShell.ast.UpdateDictionaryStatement
+import org.kevoree.tools.marShell.ast.Script
+import scala.Some
 
 object AdaptationModelWrapper {
 
--- org.kevoree.iot.modelTransform/src/main/scala/org.kevoree.iot.modelTransform/KevScriptWrapper.scala
+++ org.kevoree.iot.modelTransform/src/main/scala/org.kevoree.iot.modelTransform/KevScriptWrapper.scala
@@ -1,48 +1,15 @@
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * 	http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.gnu.org/licenses/lgpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.kevoree.iot.modelTransform
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jed
+ * Date: 08/10/13
+ * Time: 11:33
+ * To change this template use File | Settings | File Templates.
+ */
+
 
 import ast._
 import org.kevoree.tools.marShell.ast._
@@ -51,6 +18,9 @@ import collection.immutable.HashSet
 import org.kevoree.{ContainerRoot}
 import scala.collection.JavaConversions._
 import org.kevoree.log.Log
+
+
+
 
 object KevScriptWrapper {
 
@@ -266,7 +236,6 @@ object KevScriptWrapper {
     }
     val content = new StringBuilder
     var first = true
-    import scala.collection.JavaConversions._
     dictionary.foreach {
       dic =>
         if (first) {
