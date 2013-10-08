ffe3308b5af486180fa77f11ad1b3ad66b9bd798
JeD <jedartois@gmail.com>
2013-10-08 10:20:24 +0200

    update resolve c++

--- maven-resolver-cpp/src/test.cpp
+++ maven-resolver-cpp/src/test.cpp
@@ -8,7 +8,6 @@ int main(int argc, char **argv) {
 	MavenResolver *resolver = new MavenResolver();
 	
 	
-	
 	std::vector<std::string> urls;
 	urls.push_back("http://oss.sonatype.org/content/groups/public");
 	
