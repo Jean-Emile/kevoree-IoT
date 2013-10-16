01ba3879317f2deaa0546bd4ba5ac82827c6034b
kYc0o <fco.acosta@ieee.org>
2013-10-16 17:27:39 +0200

    debug mode on, ROM full

--- org.kevoree.iot.firmware/src/main/c/firmware-kevoree/server-client-coap.c
+++ org.kevoree.iot.firmware/src/main/c/firmware-kevoree/server-client-coap.c
@@ -35,7 +35,7 @@
 #include "er-coap-13.h"
 #include "er-coap-13-engine.h"
 
-#define DEBUG 0
+#define DEBUG 1
 #if DEBUG
 #define PRINTF(...) printf(__VA_ARGS__)
 #define PRINT6ADDR(addr) PRINTF("[%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x]", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
@@ -576,6 +576,7 @@ PROCESS_THREAD(kevoree_runtime, ev, data)
     PRINTF("LL header: %u\n", UIP_LLH_LEN);
     PRINTF("IP+UDP header: %u\n", UIP_IPUDPH_LEN);*/
     PRINTF("REST max chunk: %u\n", REST_MAX_CHUNK_SIZE);
+    printf("PORT: %d", REMOTE_PORT);
 
     rest_init_engine();
 
